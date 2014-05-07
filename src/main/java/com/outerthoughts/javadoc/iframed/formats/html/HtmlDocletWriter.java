0f3541c8172a2a27f1cb90e3ea2e1a982ce49230
Alexandre Rafalovitch <arafalov@gmail.com>
2014-05-07 18:07:58 +0700

    Implemented #1 Google Analytics

--- src/main/java/com/outerthoughts/javadoc/iframed/formats/html/HtmlDocletWriter.java
+++ src/main/java/com/outerthoughts/javadoc/iframed/formats/html/HtmlDocletWriter.java
@@ -423,6 +423,8 @@ public class HtmlDocletWriter extends HtmlDocWriter {
         head.addContent(getJQueryProperties());
         head.addContent(getSelect2Properties());
         head.addContent(getScriptProperties());
+        head.addContent(getAnalytics());
+        
         Content htmlTree = HtmlTree.HTML(configuration.getLocale().getLanguage(),
                 head, body);
         Content htmlDocument = new HtmlDocument(htmlDocType,
@@ -2162,5 +2164,27 @@ public class HtmlDocletWriter extends HtmlDocWriter {
      */
     public Configuration configuration() {
         return configuration;
+    }
+
+    public Content getAnalytics() {
+        String scriptCode =
+                "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n" +
+                        "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n" +
+                        "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n" +
+                        "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n" +
+                        "ga('create', '" +
+                        configuration.getResource("ga.code") +
+                        "', '" +
+                        configuration.getResource("ga.domain") +
+                        "');\n" +
+                        "ga('send', 'pageview');"
+                ;
+        Content scriptContent = new RawHtml(scriptCode);
+
+        HtmlTree ga = new HtmlTree(HtmlTag.SCRIPT);
+        ga.addAttr(HtmlAttr.TYPE, "text/javascript");
+        ga.addContent(scriptContent);
+
+        return ga;
     }
 }
